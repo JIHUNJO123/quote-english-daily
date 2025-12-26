8668ec1d67b77eb73fa36a3a6a5396278495acc6
JIHUNJO123 <jihunjo123@gmail.com>
2025-12-27 04:48:33 +0900

    Add keystore config for release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,17 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -34,20 +43,27 @@ android {
 
     signingConfigs {
         create("release") {
-            val keystorePath = System.getenv("CM_KEYSTORE_PATH")
-            if (keystorePath != null) {
-                storeFile = file(keystorePath)
+            val cmKeystorePath = System.getenv("CM_KEYSTORE_PATH")
+            if (cmKeystorePath != null) {
+                // Codemagic 환경
+                storeFile = file(cmKeystorePath)
                 storePassword = System.getenv("CM_KEYSTORE_PASSWORD")
                 keyAlias = System.getenv("CM_KEY_ALIAS")
                 keyPassword = System.getenv("CM_KEY_PASSWORD")
+            } else if (keystorePropertiesFile.exists()) {
+                // 로컬 환경
+                storeFile = file("upload-keystore.jks")
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
             }
         }
     }
 
     buildTypes {
         release {
-            val keystorePath = System.getenv("CM_KEYSTORE_PATH")
-            if (keystorePath != null) {
+            val cmKeystorePath = System.getenv("CM_KEYSTORE_PATH")
+            if (cmKeystorePath != null || keystorePropertiesFile.exists()) {
                 signingConfig = signingConfigs.getByName("release")
             } else {
                 signingConfig = signingConfigs.getByName("debug")
